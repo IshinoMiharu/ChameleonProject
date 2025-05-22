bbf56f87ac183013f8a8f29062e2fa1dcd160c89
KNTy1018 <soramametaro1018@gmail.com>
2025-05-22 13:26:39 +0900

    [update]ランキングについてのスクリプトにコメントを書き込んだ。中身に変更はない

--- Assets/Script/RankingManager.cs
+++ Assets/Script/RankingManager.cs
@@ -6,24 +6,24 @@ using UnityEngine.UI;
 
 public class RankingManager : MonoBehaviour
 {
-    private static List<int> rankingData = Enumerable.Repeat(0, 3).ToList();
+    private static List<int> rankingData = Enumerable.Repeat(0, 3).ToList();//スコアを保持するリスト
 
-    public static void AddRankingData(int data)
+    public static void AddRankingData(int data)//スコアを保持するためのメソッド
     {
-        rankingData.Add(data);
+        rankingData.Add(data);//リストにスコアを追加
 
-        rankingData = rankingData.OrderBy(n => -n).Take(3).ToList();
+        rankingData = rankingData.OrderBy(n => -n).Take(3).ToList();//リストの中身を降順にソート
     }
 
-    [SerializeField] private Transform textGroup;
-    private Text[] texts;
+    [SerializeField] private Transform textGroup;//テキストのオブジェクトを持つオブジェクトを取得
+    private Text[] texts;//テキストオブジェクトを配列で保持
 
     private void Start()
     {
-        texts =textGroup.GetComponentsInChildren<Text>();
+        texts =textGroup.GetComponentsInChildren<Text>();//textGroupが持つ子オブジェクトをすべて取得
         for(int i = 0; i < 3; i++)
         {
-            texts[i].text = $"{i + 1}:{rankingData[i].ToString().PadLeft(5, '0')}";
+            texts[i].text = $"{i + 1}:{rankingData[i].ToString().PadLeft(5, '0')}";//rankingDataが持つスコアを表示
         }
     }
 }
